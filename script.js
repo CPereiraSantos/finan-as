d5e9081a163aee67baa01645fb09e5826d459b83
CPereiraSantos <lxgames3@gmail.com>
2026-04-17 15:49:38 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,62 @@
-// script.js MÊS A MÊS
-
-const API_URL = "https://script.google.com/macros/s/AKfycbzR5t1Stifd0iVyN7U1g8KEMxzpabFekQt1TWpU3DaF-RulKWbAF9yH8BfYLuHhreDb/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzT33TqCjIjm8ojUWm9bCY439hx7hyp5cIfedfU0FeDZ275osfJp0DvmG_UZIDheXns/exec";
 
 let salarioCaio = 0;
 let salarioVictoria = 0;
 let despesas = [];
 
-function moeda(v){
-return v.toLocaleString('pt-BR',{
-style:'currency',
-currency:'BRL'
+function moeda(valor){
+return Number(valor).toLocaleString("pt-BR",{
+style:"currency",
+currency:"BRL"
 });
 }
 
 async function carregar(){
 
+try{
+
 const req = await fetch(API_URL);
 const dados = await req.json();
 
-let mes = document.getElementById("mesSelecionado").value;
-
-despesas = [];
-
-dados.despesas.forEach(item=>{
-
-let data = new Date(item.data);
-let mesItem = data.getMonth()+1;
-
-if(mesItem == mes){
-
-despesas.push({
-nome:item.descricao,
-valor:Number(item.valor)
-});
-
-}
-
-});
-
-salarioCaio = Number(dados.salarios[0].salario);
-salarioVictoria = Number(dados.salarios[1].salario);
+despesas = dados.despesas || [];
+salarioCaio = Number(dados.salarios[0]?.salario || 0);
+salarioVictoria = Number(dados.salarios[1]?.salario || 0);
 
 document.getElementById("salarioCaio").value = salarioCaio;
 document.getElementById("salarioVictoria").value = salarioVictoria;
 
 atualizar();
 
+}catch(erro){
+console.log("Erro ao carregar:", erro);
+}
+
 }
 
 function atualizar(){
 
 let totalSalarios = salarioCaio + salarioVictoria;
-let totalDespesas = despesas.reduce((a,b)=>a+b.valor,0);
+
+let totalDespesas = despesas.reduce((total,item)=>{
+return total + Number(item.valor);
+},0);
+
 let divisao = totalDespesas / 2;
-let sobra = totalSalarios - totalDespesas;
+let sobraCasa = totalSalarios - totalDespesas;
+
+let sobraCaio = salarioCaio - divisao;
+let sobraVictoria = salarioVictoria - divisao;
 
 document.getElementById("salarios").innerText = moeda(totalSalarios);
 document.getElementById("despesas").innerText = moeda(totalDespesas);
-document.getElementById("sobra").innerText = moeda(sobra);
+document.getElementById("sobra").innerText = moeda(sobraCasa);
 document.getElementById("divisao").innerText = moeda(divisao);
 
-document.getElementById("caioSobra").innerText = moeda(salarioCaio-divisao);
-document.getElementById("vicSobra").innerText = moeda(salarioVictoria-divisao);
+document.getElementById("salCaio").innerText = moeda(salarioCaio);
+document.getElementById("salVictoria").innerText = moeda(salarioVictoria);
+
+document.getElementById("caioSobra").innerText = moeda(sobraCaio);
+document.getElementById("vicSobra").innerText = moeda(sobraVictoria);
 
 let html = "";
 
@@ -69,7 +64,7 @@ despesas.slice().reverse().forEach(item=>{
 
 html += `
 <li>
-<span>${item.nome}</span>
+<span>${item.descricao}</span>
 <strong>${moeda(item.valor)}</strong>
 </li>
 `;
@@ -113,11 +108,4 @@ carregar();
 
 }
 
-window.onload = function(){
-
-let hoje = new Date().getMonth()+1;
-document.getElementById("mesSelecionado").value = hoje;
-
-carregar();
-
-}
+window.onload = carregar;
